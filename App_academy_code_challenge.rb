fce5c95bf4d6e1891a85518d1cd80635fd6f1898
FrancoB411 <Francob411@gmail.com>
2012-10-08 23:16:13 -0400

    Add Convenience method to pass tests with both floating and integer average results.

--- App_academy_code_challenge.rb
+++ App_academy_code_challenge.rb
@@ -6,23 +6,13 @@ def average(numbers)
   (sum / numbers.count)
 end
 
-average([5]) == 5
-average([5, 15]) == 10
-average([5, 10, 15]) == 10
-average([5, 6]) == 5.5
-
-
 # Write a method that creates a cat
 
 def make_cat(cat_name, home_city, age)
   { "name"=> cat_name, "home_city" => home_city, "age" => age }
 end
 
-fluffy = make_cat("Breakfast", "San Francisco", 10) == {
-  "name" => "Breakfast",
-  "home_city" => "San Francisco",
-  "age" => 10
-}
+
 
 # Write a method that computes the average cat age
 
@@ -31,14 +21,8 @@ def average_cat_age(cats)
    for cat in cats do
      numbers << cat["age"].to_f
    end
-   average(numbers).to_i
+   intergize(average(numbers))  # new new method added
 end
-
-# tests
-cat1 = make_cat("Breakfast", "San Francisco", 10)
-cat2 = make_cat("Earl", "San Francisco", 2)
-cat3 = make_cat("Gizmo", "Albany", 12)
-average_cat_age([cat1, cat2, cat3]) == 8
 
 # Write a method that computes the number of cats in each city
 
@@ -54,7 +38,41 @@ def cats_per_city(cats)
  cities
 end
 
-# tests
+# convenience method that formats float ending in .0 into integers
+def intergize(num)
+  if num%1 == 0.0
+    num.to_i
+  else
+    num
+  end
+end
+
+# tests: Find the average
+average([5]) == 5
+average([5, 15]) == 10
+average([5, 10, 15]) == 10
+average([5, 6]) == 5.5
+
+# tests: Write a method that creates a cat
+
+fluffy = make_cat("Breakfast", "San Francisco", 10) == {
+  "name" => "Breakfast",
+  "home_city" => "San Francisco",
+  "age" => 10
+}
+
+# tests: Write a method that computes the average cat age
+cat1 = make_cat("Breakfast", "San Francisco", 10)
+cat2 = make_cat("Earl", "San Francisco", 2)
+cat3 = make_cat("Gizmo", "Albany", 12)
+average_cat_age([cat1, cat2, cat3]) == 8
+
+cat4 = make_cat("Breakfast", "San Francisco", 5.5)
+cat5 = make_cat("Earl", "San Francisco", 5.5)
+cat6 = make_cat("Gizmo", "Albany", 5.5)
+average_cat_age([cat4, cat5, cat6]) == 5.5
+
+# tests Write a method that computes the number of cats in each city
 cat2 = make_cat("Earl", "San Francisco", 2)
 cat3 = make_cat("Gizmo", "Albany", 12)
 cats_per_city([cat1, cat2, cat3]) == {
